4e288c8c368a1bc13f72ad11c4ffe7a5acfbb27a
unknown <campuzanobedoyadaniel@gmail.com>
2020-11-06 00:17:10 -0500

    Se agregan cors

--- src/main/java/com/experimentality/changinglifequotes/models/controller/QuoteRestController.java
+++ src/main/java/com/experimentality/changinglifequotes/models/controller/QuoteRestController.java
@@ -1,6 +1,7 @@
 package com.experimentality.changinglifequotes.models.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import com.experimentality.changinglifequotes.models.service.IQuoteService;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:4200")
 public class QuoteRestController {
 
 	@Autowired
